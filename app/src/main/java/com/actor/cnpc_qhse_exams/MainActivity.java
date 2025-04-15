01817def5c4676e2b6c62b7ccf846a6daa5c5684
actor20170211030627 <1455198886@qq.com>
2025-04-15 17:04:16 +0800

    first commit

---
+++ app/src/main/java/com/actor/cnpc_qhse_exams/MainActivity.java
@@ -0,0 +1,15 @@
+package com.actor.cnpc_qhse_exams;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    //交通安全基层站队QHSE标准化建设—驾驶员应知应会题库
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
